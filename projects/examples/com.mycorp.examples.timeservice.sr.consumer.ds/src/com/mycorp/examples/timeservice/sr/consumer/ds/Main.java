5f4e132d3021e28d3d7c70452ad8b2fe864ae8fc
slewis <slewis@composent.com>
2017-12-27 15:55:00 -0800

    Fixes for remote service examples

--- projects/examples/com.mycorp.examples.timeservice.sr.consumer.ds/src/com/mycorp/examples/timeservice/sr/consumer/ds/Main.java
+++ projects/examples/com.mycorp.examples.timeservice.sr.consumer.ds/src/com/mycorp/examples/timeservice/sr/consumer/ds/Main.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016 Composent, Inc. and others. All rights reserved. This
+ * Copyright (c) 2017 Composent, Inc. and others. All rights reserved. This
  * program and the accompanying materials are made available under the terms of
  * the Eclipse Public License v1.0 which accompanies this distribution, and is
  * available at http://www.eclipse.org/legal/epl-v10.html
@@ -13,10 +13,12 @@ import java.util.ServiceLoader;
 
 import org.eclipse.ecf.osgi.serviceregistry.ServiceRegistry;
 import org.eclipse.ecf.osgi.serviceregistry.ServiceRegistryFactory;
+import org.eclipse.ecf.osgi.services.remoteserviceadmin.DebugRemoteServiceAdminListener;
 import org.eclipse.ecf.osgi.services.remoteserviceadmin.EndpointDescriptionReader;
 import org.osgi.service.remoteserviceadmin.EndpointDescription;
 import org.osgi.service.remoteserviceadmin.ImportRegistration;
 import org.osgi.service.remoteserviceadmin.RemoteServiceAdmin;
+import org.osgi.service.remoteserviceadmin.RemoteServiceAdminListener;
 
 import com.mycorp.examples.timeservice.ITimeService;
 
@@ -24,31 +26,35 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		// Create service registry
-		ServiceRegistry serviceRegistry = ServiceLoader.load(ServiceRegistryFactory.class)
-				.iterator().next().newServiceRegistry(null);
+		ServiceRegistry serviceRegistry = ServiceLoader.load(ServiceRegistryFactory.class).iterator().next()
+				.newServiceRegistry(null);
 
-		// Get RSA instance
-		RemoteServiceAdmin rsa = serviceRegistry.getService(serviceRegistry.getServiceReference(RemoteServiceAdmin.class));
-		
-		// Read endpoint description from file
+		// This service registration produces RSA export registration info to console
+		serviceRegistry.registerService(RemoteServiceAdminListener.class, new DebugRemoteServiceAdminListener(), null);
+
+		// Get RemoteServiceAdmin instance
+		RemoteServiceAdmin rsa = serviceRegistry
+				.getService(serviceRegistry.getServiceReference(RemoteServiceAdmin.class));
+
+		// Read endpoint description from edef file
 		EndpointDescription ed = new EndpointDescriptionReader()
-		.readEndpointDescriptions(Main.class
-				.getResourceAsStream("timeserviceendpointdescription.xml"))[0];
-		
+				.readEndpointDescriptions(Main.class.getResourceAsStream("timeserviceendpointdescription.xml"))[0];
+
 		// Import the remote service using endpoint description
+		System.out.println("Consumer:  Importing remote time service");
 		ImportRegistration reg = rsa.importService(ed);
-		System.out.println("import registration="+reg);
 		// Check for import error
 		Throwable t = reg.getException();
 		if (t != null) {
+			// if import exception print stack trace and throw
 			t.printStackTrace();
-			throw new Exception("Could not import remote service",t);
+			throw new Exception("Could not import remote service", t);
 		}
-		// No error, so we are good to use the imported service
-		// Get imported service instance
-		ITimeService ts = (ITimeService)serviceRegistry.getService(reg.getImportReference().getImportedService());
-		// And then call it
-		System.out.println("time service result="+ts.getCurrentTime());
+		// No import error, so we are good to use the imported service
+		System.out.println("Consumer:  Remote time service imported. Import Registration=" + reg);
+		// Get imported service instance from service registry with registration
+		ITimeService ts = (ITimeService) serviceRegistry.getService(reg.getImportReference().getImportedService());
+		System.out.print("Consumer:  Calling remote ITimeService.getCurrentTime()=" + ts.getCurrentTime());
 	}
 
 }
--- projects/examples/com.mycorp.examples.timeservice.sr.host/src/com/mycorp/examples/timeservice/sr/host/Main.java
+++ projects/examples/com.mycorp.examples.timeservice.sr.host/src/com/mycorp/examples/timeservice/sr/host/Main.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016 Composent, Inc. and others. All rights reserved. This
+ * Copyright (c) 2017 Composent, Inc. and others. All rights reserved. This
  * program and the accompanying materials are made available under the terms of
  * the Eclipse Public License v1.0 which accompanies this distribution, and is
  * available at http://www.eclipse.org/legal/epl-v10.html
@@ -9,28 +9,65 @@
  ******************************************************************************/
 package com.mycorp.examples.timeservice.sr.host;
 
+import java.util.Hashtable;
 import java.util.ServiceLoader;
 
 import org.eclipse.ecf.osgi.serviceregistry.ServiceRegistry;
 import org.eclipse.ecf.osgi.serviceregistry.ServiceRegistryFactory;
+import org.eclipse.ecf.osgi.services.remoteserviceadmin.DebugRemoteServiceAdminListener;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.remoteserviceadmin.RemoteServiceAdminListener;
+
+import com.mycorp.examples.timeservice.ITimeService;
 
 public class Main {
 
-	@SuppressWarnings("unused")
 	private static ServiceRegistry serviceRegistry;
 
+	// Here is the ITimeService implementation class
+	public static class TimeServiceImpl implements com.mycorp.examples.timeservice.ITimeService {
+
+		@Override
+		public Long getCurrentTime() {
+			Long result = System.currentTimeMillis();
+			System.out.println("getCurrentTime called on host.  Returning time=" + result);
+			return result;
+		}
+
+	}
+
 	public static void main(String[] args) throws Exception {
+		// Create service registry using service loader and ServiceRegistryFactory
+		serviceRegistry = ServiceLoader.load(ServiceRegistryFactory.class).iterator().next().newServiceRegistry(null);
 
-		serviceRegistry = ServiceLoader.load(ServiceRegistryFactory.class)
-				.iterator().next().newServiceRegistry(null);
+		// This produces RSA export registration info to console for example
+		serviceRegistry.registerService(RemoteServiceAdminListener.class, new DebugRemoteServiceAdminListener(), null);
 
+		// Create and set service properties with RS-required values
+		Hashtable<String, String> props = new Hashtable<String, String>();
+		props.put("service.exported.interfaces", "*");
+		// For this example, will use the ecf.generic.server provider.
+		// See http://wiki.eclipse.org/Distribution_Providers for list of OS providers
+		props.put("service.exported.configs", "ecf.generic.server");
+		// For this ecf.generic.server provider, hostname and port can be set via
+		// properties
+		props.put("ecf.generic.server.hostname", "localhost");
+		props.put("ecf.generic.server.port", "3288");
+		// Register with RS properties that will trigger RSA to export as part of
+		// registration
+		System.out.println("Service Host:  Registering remote implementation of ITimeService");
+		ServiceRegistration<ITimeService> reg = serviceRegistry.registerService(ITimeService.class,
+				new TimeServiceImpl(), props);
+		System.out.println("Service Host:  Remote implementation of ITimeService registered.  Registration=" + reg);
+
+		// Then main thread waits forever for remote service calls to
+		// TimeServiceImpl.getCurrentTime()
 		Object w = new Object();
 		synchronized (w) {
 			// wait forever
 			try {
 				w.wait();
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
